a916e6f87f519e81c7259f52230c3d9ac061b2af
ndbarbanera <ndbarbanera@users.noreply.github.com>
2015-03-10 22:15:44 +0100

    script 1

--- script01_01.sh
+++ script01_01.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
-# Riceve un argomento da riga di comando.
-# Se la directory corrente contiene il file il cui nome e' passato come argomento, stampa "File trovato!", altrimenti stampa "File _non_ trovato"
+if [ $# -gt 1 -o $# -eq 0 ]
+then
+	echo
+	echo "Inserire uno ed un solo argomento!"
+	exit 1
+fi
+
+
+
+echo
+
+
+if [ -f $@ ]
+then
+	echo "File trovato!"
+else
+	echo "File_non_trovato"
+fi
+
+exit 0
+
+
+
+
+
